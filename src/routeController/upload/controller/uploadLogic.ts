f3c6bc330747e09fdeca4c578a0bc6a33fda9e64
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-09-09 23:18:50 +0530

    FEAT : login error fixed

--- src/routeController/upload/controller/uploadLogic.ts
+++ src/routeController/upload/controller/uploadLogic.ts
@@ -1,3 +1,4 @@
+import logger from "../../../../config/winstion";
 import supabase from "../../../middleware/supabase";
 
 const uploadSupabase = async (req, res) => {
@@ -9,17 +10,16 @@ const uploadSupabase = async (req, res) => {
     const { originalname, buffer } = req.file;
     const { data, error } = await supabase.storage
       .from("evc") // Replace with your bucket name
-      .upload(`public/${originalname}`, buffer, {
-        contentType: req.file.mimetype,
-      });
+      .upload(`${originalname}`, req.file[0]);
 
     if (error) {
+      logger.error(error);
       throw error;
     }
 
     res.status(200).json({
       message: "File uploaded successfully",
-      fileUrl: process.env.BASE_URL + originalname,
+      fileUrl: process.env.BASE_URL + data.path,
     });
   } catch (err) {
     res.status(500).json({ message: "Upload failed", error: err.message });
